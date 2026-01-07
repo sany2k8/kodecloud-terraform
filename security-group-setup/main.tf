d7aded9277ff3f27c24d37b6e83b49f6e830ea69
Sany <sany2k8@gmail.com>
2026-01-07 17:42:19 +0600

    Add vpc with ipv4 cidr

---
+++ security-group-setup/main.tf
@@ -0,0 +1,38 @@
+# Fetch the default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Security Group for Nautilus App Servers
+resource "aws_security_group" "devops_sg" {
+  name        = "devops-sg"
+  description = "Security group for Nautilus App Servers"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "HTTP access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "devops-sg"
+  }
+}
--- vpc-with-ipv4-cidr/task.md
+++ vpc-with-ipv4-cidr/task.md
@@ -1,3 +1,3 @@
 # Create a VPC named
-- devops-vpc in region us-east-1 with any IPv4 CIDR block through terraform. 
+- nautilus-vpc in us-east-1 region with 192.168.0.0/24 IPv4 CIDR using terraform. 
 - The Terraform working directory is /home/bob/terraform. Create the main.tf file (do not create a different .tf file) to accomplish this task.

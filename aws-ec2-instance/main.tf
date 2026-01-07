e9f7d6e7a8e6d911aea08259a5d2f4fa1c3d2f37
Md. Sany Ahmed <sany2k8@gmail.com>
2026-01-07 16:41:41 +0600

    Add terraform main for ec2 instance

---
+++ aws-ec2-instance/main.tf
@@ -0,0 +1,19 @@
+resource "tls_private_key" "devops_kp" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+resource "aws_key_pair" "devops_kp" {
+  key_name   = "devops-kp"
+  public_key = tls_private_key.devops_kp.public_key_openssh
+}
+
+resource "aws_instance" "devops_ec2" {
+  ami           = "ami-0c101f26f147fa7fd"
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.devops_kp.key_name
+
+  tags = {
+    Name = "devops-ec2"
+  }
+}

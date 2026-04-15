c28246b7bb8b29a6e87d60cd9b841a7e707f6884
Md. Sany Ahmed <sany2k8@gmail.com>
2026-04-15 23:33:26 +0600

    Refactor Kinesis stream resource configuration
    Removed retention period and tags from Kinesis stream resource.

--- aws-kinesis-stream/main.tf
+++ aws-kinesis-stream/main.tf
@@ -1,14 +1,8 @@
 resource "aws_kinesis_stream" "devops_stream" {
-  name             = "devops-stream"
-  shard_count      = 1
-  retention_period = 24
+  name        = "devops-stream"
+  shard_count = 1
 
   stream_mode_details {
     stream_mode = "PROVISIONED"
-  }
-
-  tags = {
-    Name        = "devops-stream"
-    Environment = "dev"
   }
 }
